492263c7842d2e1127c38e93dacdc9c537f8826c
Jeramy Morrill <jeramymorrill@bossaimac01-jeramy.local>
2015-04-09 11:50:33 -0400

    adding constants

--- src/unity/Ahab's Magic Box/Assets/Scripts/Shooter.cs
+++ src/unity/Ahab's Magic Box/Assets/Scripts/Shooter.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Shooter : MonoBehaviour {
 
+	//private List<GameObject> spheres = new List<GameObject>();
+	private GameObject sphere;
 	// Use this for initialization
 	void Start () {
-	
+		sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		sphere.transform.position = sphere.transform.position + new Vector3 (0, 0, .8f);
+		if (sphere.transform.position.z > 150) {
+			sphere.transform.position = Vector3.zero;
+		}
 	}
 }
--- src/unity/Ahab's Magic Box/Assets/Scripts/SplitDisplay.cs
+++ src/unity/Ahab's Magic Box/Assets/Scripts/SplitDisplay.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class SplitDisplay : MonoBehaviour {
 
+	private const float CAM_Y = 1.0f;
+	private const float CAM_Z_INC = 30.0f;
+	private const float CAM_CLIP_OVERLAP = 10.0f;
+
 	public RenderTexture camTexture1;
 	public RenderTexture camTexture2;
 	public RenderTexture camTexture3;
@@ -12,9 +16,12 @@ public class SplitDisplay : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		cam1.transform.position = new Vector3(0, 0, 0);
-		cam2.transform.position = new Vector3(0, 0, 300);
-		cam3.transform.position = new Vector3(0, 0, 600);
+		cam1.transform.position = new Vector3(0, CAM_Y, 0);
+		cam2.transform.position = new Vector3(0, CAM_Y, CAM_Z_INC);
+		cam3.transform.position = new Vector3(0, CAM_Y, CAM_Z_INC * 2);
+		cam1.farClipPlane = CAM_Z_INC + CAM_CLIP_OVERLAP;
+		cam2.farClipPlane = CAM_Z_INC + CAM_CLIP_OVERLAP;
+		cam3.farClipPlane = CAM_Z_INC + CAM_CLIP_OVERLAP;
 	}
 	
 	// Update is called once per frame
