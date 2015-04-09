48ecfd71cbcc6b4c774f993abc64f7445e44ffce
Jeramy Morrill <jeramymorrill@bossaimac01-jeramy.local>
2015-04-09 15:31:38 -0400

    changed clipping method

--- src/unity/Ahab's Magic Box/Assets/Scripts/Shooter.cs
+++ src/unity/Ahab's Magic Box/Assets/Scripts/Shooter.cs
@@ -3,17 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Shooter : MonoBehaviour {
-
-	//private List<GameObject> spheres = new List<GameObject>();
 	private GameObject sphere;
-	// Use this for initialization
+
 	void Start () {
 		sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		sphere.transform.localScale = new Vector3 (3, 3, 3);
 	}
-	
-	// Update is called once per frame
 	void Update () {
-		sphere.transform.position = sphere.transform.position + new Vector3 (0, 0, .2f);
+		sphere.transform.position = sphere.transform.position + new Vector3 (0, 0, 1f);
 		if (sphere.transform.position.z > 100) {
 			sphere.transform.position = Vector3.zero;
 		}
--- src/unity/Ahab's Magic Box/Assets/Scripts/SplitDisplay.cs
+++ src/unity/Ahab's Magic Box/Assets/Scripts/SplitDisplay.cs
@@ -5,7 +5,8 @@ public class SplitDisplay : MonoBehaviour {
 
 	private const float CAM_Y = 1.0f;
 	private const float CAM_Z_INC = 30.0f;
-	private const float CAM_CLIP_OVERLAP = 10.0f;
+	private const float CAM_CLIP_OVERLAP = 0.1f;
+
 
 	public RenderTexture camTexture1;
 	public RenderTexture camTexture2;
@@ -13,15 +14,23 @@ public class SplitDisplay : MonoBehaviour {
 	public Camera cam1;
 	public Camera cam2;
 	public Camera cam3;
+	public bool flipOrder = true;
+
 
 	// Use this for initialization
 	void Start () {
-		cam1.transform.position = new Vector3(0, CAM_Y, 0);
-		cam2.transform.position = new Vector3(0, CAM_Y, CAM_Z_INC);
-		cam3.transform.position = new Vector3(0, CAM_Y, CAM_Z_INC * 2);
+		cam1.transform.position = new Vector3 (0, CAM_Y, 0);
+		cam2.transform.position = new Vector3 (0, CAM_Y, 0);
+		cam3.transform.position = new Vector3 (0, CAM_Y, 0);
+
+		cam1.nearClipPlane = 0.1f;
 		cam1.farClipPlane = CAM_Z_INC + CAM_CLIP_OVERLAP;
-		cam2.farClipPlane = CAM_Z_INC + CAM_CLIP_OVERLAP;
-		cam3.farClipPlane = CAM_Z_INC + CAM_CLIP_OVERLAP + 600;
+
+		cam2.nearClipPlane = CAM_Z_INC;
+		cam2.farClipPlane = (CAM_Z_INC * 2) + CAM_CLIP_OVERLAP;
+
+		cam1.nearClipPlane = (CAM_Z_INC * 2);
+		cam1.farClipPlane = (CAM_Z_INC * 3) + CAM_CLIP_OVERLAP;
 	}
 	
 	// Update is called once per frame
@@ -30,9 +39,17 @@ public class SplitDisplay : MonoBehaviour {
 	}
 
 	void OnGUI() {
-		float thirdScreenHieght = Screen.height / 3;
-		GUI.DrawTexture(new Rect(0, thirdScreenHieght *2, Screen.width, thirdScreenHieght), camTexture1, ScaleMode.ScaleAndCrop, true, 0);
-		GUI.DrawTexture(new Rect(0, thirdScreenHieght, Screen.width, thirdScreenHieght), camTexture2, ScaleMode.ScaleAndCrop, true, 0);
-		GUI.DrawTexture(new Rect(0, 0, Screen.width, thirdScreenHieght), camTexture3, ScaleMode.ScaleAndCrop, true, 0);
+		if (Event.current.type.Equals (EventType.Repaint)) {
+			float thirdScreenHieght = Screen.height / 3;
+			if (flipOrder) {
+				GUI.DrawTexture (new Rect (0, thirdScreenHieght * 2, Screen.width, thirdScreenHieght), camTexture1, ScaleMode.ScaleAndCrop, true, 0);
+				GUI.DrawTexture (new Rect (0, thirdScreenHieght, Screen.width, thirdScreenHieght), camTexture2, ScaleMode.ScaleAndCrop, true, 0);
+				GUI.DrawTexture (new Rect (0, 0, Screen.width, thirdScreenHieght), camTexture3, ScaleMode.ScaleAndCrop, true, 0);
+			} else {
+				GUI.DrawTexture (new Rect (0, 0, Screen.width, thirdScreenHieght), camTexture1, ScaleMode.ScaleAndCrop, true, 0);
+				GUI.DrawTexture (new Rect (0, thirdScreenHieght, Screen.width, thirdScreenHieght), camTexture2, ScaleMode.ScaleAndCrop, true, 0);
+				GUI.DrawTexture (new Rect (0, thirdScreenHieght * 2, Screen.width, thirdScreenHieght), camTexture3, ScaleMode.ScaleAndCrop, true, 0);
+			}
+		}
 	}
 }
