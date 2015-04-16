99e2eb860fd0059a7a105b8a518a969dc34423e1
Jeramy Morrill <jeramymorrill@bossaimac01-jeramy.local>
2015-04-16 14:42:32 -0400

    projectile factory started

---
+++ src/unity/Ahab's Magic Box/Assets/Scripts/ProjectileFactory.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileFactory : MonoBehaviour {
+
+	public GameObject projectile;
+
+	public int speed = 80;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			CreateProjectile();
+		}
+
+	}
+
+	void ExpellProjectile(){
+		CreateProjectile ();
+		//Debug.Log (_pj);
+		//_pj.velocity = transform.TransformDirection(Vector3.forward * 10);
+	}
+
+	void CreateProjectile(){
+		GameObject clone;
+		clone = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+		clone.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.forward * speed);
+	}
+}
